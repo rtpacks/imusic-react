6dbcac44e949b3574153d0e74cc4aefae3257372
azin-cn <luckywords@foxmail.com>
2022-08-01 14:14:36 +0800

    新增：Image组件

---
+++ src/components/common/Image/index.jsx
@@ -0,0 +1,25 @@
+import React, { memo } from "react";
+
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  width: 100%;
+  height: 150px;
+  border-radius: ${(p) => p.radius || "12px"};
+  box-shadow: 0 0 4px 2px #ccc;
+  img {
+    width: 100%;
+  }
+`;
+
+const Image = memo((props) => {
+  const { img, alt = "Hello" } = props;
+  return (
+    <Wrapper>
+      <img src={img} alt={alt} />
+    </Wrapper>
+  );
+});
+
+export default Image;
