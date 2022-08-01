c8ca5d8fd37f5ebba9072de0440d671c80ae73ae
azin-cn <luckywords@foxmail.com>
2022-08-01 23:30:27 +0800

    更新：IContent组件，调整细节

--- src/components/content/IContent/index.jsx
+++ src/components/content/IContent/index.jsx
@@ -1,19 +1,37 @@
 import React, { memo } from "react";
 import styled from "@emotion/styled";
 
+import NavHeader from "../../common/NavHeader";
 import Card from "../../common/Card";
+import { ThemeProvider } from "@emotion/react";
 
-import style from "./style.module.less";
+const H2 = styled.h2`
+  width: 96%;
+  margin: 4px auto;
+  padding: 0 4px;
+  font-size: 26px;
+  font-weight: 700;
+  user-select: none;
+`;
 
 const Flex = styled.div`
   display: flex;
   justify-content: space-evenly;
   padding: 14px 0;
+  user-select: none;
 `;
+
+const theme = {
+  color: "#666",
+  activeColor: "#FF6160",
+  userSelect: "none",
+};
 
 const IContent = memo(() => {
   return (
-    <div className={style.scroll}>
+    <ThemeProvider theme={theme}>
+      <H2>推荐</H2>
+      <NavHeader />
       <Flex>
         <Card
           img="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F34%2Faf%2F78%2F34af7862028d2b7ff3ab92f7a936421e.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1661918017&t=e9af07fd367371df48c6a696284f578a"
@@ -34,7 +52,7 @@ const IContent = memo(() => {
           title="今日你的温柔你的世界，和我一起回想以前时光"
         />
       </Flex>
-
+      <NavHeader />
       <Flex>
         <Card
           width="46%"
@@ -52,7 +70,7 @@ const IContent = memo(() => {
           title="今日你的温柔你的世界，和我一起回想以前时光"
         />
       </Flex>
-
+      <NavHeader />
       <Flex>
         <Card
           width="30%"
@@ -72,7 +90,7 @@ const IContent = memo(() => {
           title="今日你的温柔你的世界，和我一起回想以前时光"
         />
       </Flex>
-    </div>
+    </ThemeProvider>
   );
 });
 
