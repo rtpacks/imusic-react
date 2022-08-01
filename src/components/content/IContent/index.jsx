149234e9fe86fcfbc03e12772f8e384aae2dffa4
azin-cn <luckywords@foxmail.com>
2022-08-01 10:48:43 +0800

    新增：IContennt和IFooter

---
+++ src/components/content/IContent/index.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+
+import OneFourth from "../../common/OneFourth";
+
+const IContent = memo(() => {
+  return (
+    <div>
+      <OneFourth></OneFourth>
+    </div>
+  )
+})
+
+export default IContent
